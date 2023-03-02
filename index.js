250ef79086b2af3e93cc5d9fb04a7b02f64f9e35
njambi-cloud <christinemacharia64@gmail.com>
2023-03-02 05:08:37 +0300

    Adds quick sort algorithm

--- index.js
+++ index.js
@@ -40,3 +40,23 @@
 // }
 // console.log(quickSort([29,3,-5,7,19,8,5]))
 
+function quickSort(arr) {
+    if(arr.length<1){
+        return arr
+    }
+    let pivot= arr[Math.floor((arr.length)/2)]
+    let left=[]
+    let right=[]
+
+    for(let i=0; i<arr.length-1; i++){
+        if(arr[i]< pivot){
+            left.push(arr[i])
+        }
+        else{
+            right.push(arr[i])
+        }
+    }
+    return quickSort(left).concat(pivot, quickSort(right))
+}
+console.log(quickSort([29,3,-5,7,19,8,5]))
+
